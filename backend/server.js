a5534853d85394c9f977fcaa63ea35e8e2794356
Varshith Gowda <varshith@Varshiths-MacBook-Air.local>
2026-01-21 15:02:37 +0530

    Prepare app for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ const movieRoutes = require("./routes/movieRoutes");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
